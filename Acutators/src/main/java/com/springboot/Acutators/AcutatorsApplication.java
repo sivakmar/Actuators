ebe78d3dd2dfc406fa5d80086e3186a065265cfe
Siva Kumar <117735455+sivakmar@users.noreply.github.com>
2024-08-05 14:06:42 +0530

    Add files via upload

---
+++ Acutators/src/main/java/com/springboot/Acutators/AcutatorsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.Acutators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcutatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcutatorsApplication.class, args);
+	}
+
+}
---
+++ Acutators/src/test/java/com/springboot/Acutators/AcutatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.Acutators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcutatorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
